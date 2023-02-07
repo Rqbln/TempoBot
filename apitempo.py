d3d08cbf224b3b9d1825d7d3e61737a4dd64b345
Rqbln <118724817+Rqbln@users.noreply.github.com>
2023-02-07 19:54:38 +0100

    Ajout ChoisirJour() Ajout fichiers Projet PyCharm

--- apitempo.py
+++ apitempo.py
@@ -1,11 +1,22 @@
 import requests
 
-url = "https://particulier.edf.fr/services/rest/referentiel/searchTempoStore?dateRelevant=2023-02-05"
+def ChoisirJour():
+    Annee = int(input("Année : "))
+    Mois = int(input("Mois : "))
+    Jour = int(input("Jour : "))
 
-reponse = str(requests.get(url).text)
-dico = {"couleurJourJ":"TEMPO_BLEU","couleurJourJ1":"TEMPO_ROUGE"}
-couleur = dico["couleurJourJ1"]
-print(couleur)
-if ("TEMPO_ROUGE"==couleur):
-    print("cest rouge")
+    url = "https://particulier.edf.fr/services/rest/referentiel/searchTempoStore?dateRelevant={}-{}-{}".format(Annee,Mois,Jour)
+
+    reponse = requests.get(url).json()
+    couleur = reponse["couleurJourJ"]
+    if "TEMPO_ROUGE"==couleur:
+        print("Le jour est rouge")
+    elif "TEMPO_BLEU"==couleur:
+        print("Le jour est bleu")
+    elif "TEMPO_BLANC"==couleur:
+        print("Le jour est blanc")
+    elif "NON_DEFINI"==couleur:
+        print("Le jour n'est pas connu.\nVous avez sûrement défini une date antérieure au début de Tempo, ou la date est supérieure à J+2")
+
+
 
