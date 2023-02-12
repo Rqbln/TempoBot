cb3fdcee5113fc96d8a0709a138bde6e65fddad5
Rqbln <118724817+Rqbln@users.noreply.github.com>
2023-02-12 16:33:51 +0100

    Changement affichage Data + jours en Fr

--- apitempo.py
+++ apitempo.py
@@ -3,6 +3,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 import datetime
+
 cred = credentials.Certificate("tempobot-406fc-firebase-adminsdk-o6bkq-a1ab9cdc76.json")
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://tempobot-406fc-default-rtdb.europe-west1.firebasedatabase.app/'
@@ -10,48 +11,61 @@ firebase_admin.initialize_app(cred, {
 
 ref = db.reference("/data")
 
-def ChoisirJour():
-    date = datetime.date.today()
-    Annee = date.year
-    Mois = date.month
-    Jour = date.day
 
-    #Annee = int(input("Année : "))
-    #Mois = int(input("Mois : "))
-    #Jour = int(input("Jour : "))
+def ChoisirJour():
+
+    now = datetime.datetime.now()
+
+    weekday_dict = {
+        0: "Lundi",
+        1: "Mardi",
+        2: "Mercredi",
+        3: "Jeudi",
+        4: "Vendredi",
+        5: "Samedi",
+        6: "Dimanche"
+    }
+
+    weekday = weekday_dict[now.weekday()]
+
+    date = "{} {:%d-%m-%Y}".format(weekday, now)
+
+    parts = date.split("-")
+    Annee = parts[-1]
+    Mois = parts[-2]
+    Jour = parts[0].split(" ")[-1]
 
     url = "https://particulier.edf.fr/services/rest/referentiel/searchTempoStore?dateRelevant={}-{}-{}".format(Annee,Mois,Jour)
 
     reponse = requests.get(url).json()
     couleurJ = reponse["couleurJourJ"]
     couleurJ1 = reponse["couleurJourJ1"]
-    if "TEMPO_ROUGE"==couleurJ:
+    if "TEMPO_ROUGE" == couleurJ:
         print("Le jour est rouge")
-    elif "TEMPO_BLEU"==couleurJ:
+    elif "TEMPO_BLEU" == couleurJ:
         print("Le jour est bleu")
-    elif "TEMPO_BLANC"==couleurJ:
+    elif "TEMPO_BLANC" == couleurJ:
         print("Le jour est blanc")
     else:
-        print("Le jour n'est pas connu.\nVous avez sûrement défini une date antérieure au début de Tempo, ou la date est supérieure à J+2")
+        print(
+            "Le jour n'est pas connu.\nVous avez sûrement défini une date antérieure au début de Tempo, ou la date est supérieure à J+2")
+    print(date)
     heure = str(datetime.datetime.now())
-    data = {"couleurJ": couleurJ,"couleurJ1": couleurJ1,"Date": heure}
+    data = {"couleurJ": couleurJ, "couleurJ1": couleurJ1, "Date": date}
     ref.set(data)
 
 
-ChoisirJour()
-
-
 def configPrise():
-    heures=[[0, 0],[0, 0],[0, 0]]
-    couleurs=["rouges","blancs","bleus"]
+    heures = [[0, 0], [0, 0], [0, 0]]
+    couleurs = ["rouges", "blancs", "bleus"]
     print("Configuration de la prise connectée (1 pour On, 2 pour Off)")
-    for i in range (3):
+    for i in range(3):
 
-        print("Jours",couleurs[i])
-        heures[i][0]=input("Heures pleines :")
-        if "1"==heures[i][0]:
+        print("Jours", couleurs[i])
+        heures[i][0] = input("Heures pleines :")
+        if "1" == heures[i][0]:
             print("ON")
-        elif"0"==heures[i][0]:
+        elif "0" == heures[i][0]:
             print("OFF")
 
         heures[i][1] = input("Heures creuses :")
@@ -60,18 +74,18 @@ def configPrise():
         elif "0" == heures[i][1]:
             print("OFF")
 
-
     print("\nRésumé :")
     for i in range(3):
         print("Jours", couleurs[i], ":")
         print("Heures pleines :", "ON" if heures[i][0] == "1" else "OFF")
         print("Heures creuses :", "ON" if heures[i][1] == "1" else "OFF")
 
+
 def tasmotaAPI():
-    ip ="Entrez l'ip de la prise :"
-    commandON="Power1"
-    commandOFF="Power0"
-    url="http://{ip}/cm?cmnd=Status%200"
+    ip = "Entrez l'ip de la prise :"
+    commandON = "Power1"
+    commandOFF = "Power0"
+    url = "http://{ip}/cm?cmnd=Status%200"
 
     try:
         response = requests.get(url, timeout=5)
@@ -81,10 +95,15 @@ def tasmotaAPI():
             if state == "ON":
                 response = requests.get(f"http://{ip}/cm?cmnd={commandOFF}")
                 print(response.text)
-            elif (state == "OFF"):
+            elif state == "OFF":
                 response = requests.get(f"http://{ip}/cm?cmnd={commandON}")
                 print(response.text)
         else:
             print("La prise est connectée, mais impossible de récupérer son état.")
     except requests.exceptions.RequestException as e:
         print("La prise n'est pas connectée.")
+
+
+if __name__ == "__main__":
+
+    ChoisirJour()
