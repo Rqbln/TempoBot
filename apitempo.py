10af183c8090f86fc7179cb55ecab4d1b82c635f
VTRMAZ <victormazloum@gmail.com>
2023-02-12 14:31:20 +0100

    add J+1

--- apitempo.py
+++ apitempo.py
@@ -17,16 +17,17 @@ def ChoisirJour():
     url = "https://particulier.edf.fr/services/rest/referentiel/searchTempoStore?dateRelevant={}-{}-{}".format(Annee,Mois,Jour)
 
     reponse = requests.get(url).json()
-    couleur = reponse["couleurJourJ"]
-    if "TEMPO_ROUGE"==couleur:
+    couleurJ = reponse["couleurJourJ"]
+    couleurJ1 = reponse["couleurJourJ1"]
+    if "TEMPO_ROUGE"==couleurJ:
         print("Le jour est rouge")
-    elif "TEMPO_BLEU"==couleur:
+    elif "TEMPO_BLEU"==couleurJ:
         print("Le jour est bleu")
-    elif "TEMPO_BLANC"==couleur:
+    elif "TEMPO_BLANC"==couleurJ:
         print("Le jour est blanc")
     else:
         print("Le jour n'est pas connu.\nVous avez sûrement défini une date antérieure au début de Tempo, ou la date est supérieure à J+2")
-    data = {"couleur": couleur}
+    data = {"couleurJ": couleurJ,"couleurJ1": couleurJ1}
     ref.set(data)
 
 
