7ee6842f80a27da72092c0a4f0dd1ec7582379d4
Rqbln <118724817+Rqbln@users.noreply.github.com>
2023-03-08 19:44:32 +0100

    Début connexion à l'app

--- Python/testdtata.py
+++ Python/testdtata.py
@@ -1,18 +1,50 @@
-import pyrebase
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import requests
+import time
 
-config = {
-  "apiKey": "a1ab9cdc76097a51e7f2975107179f65365cf489",
-  "authDomain": "tempobot-7eea7.firebaseapp.com",
-  "databaseURL": "https://tempobot-406fc-default-rtdb.europe-west1.firebasedatabase.app/",
-  "storageBucket": "gs://tempobot-7eea7.appspot.com"
-}
+ip = "192.168.68.65"  # remplacer par l'adresse IP de votre prise Tasmota
+command_on = "Power%20on"
+command_off = "Power%20off"
 
-firebase = pyrebase.initialize_app(config)
-db = firebase.database()
+# initialiser l'application Firebase
+cred = credentials.Certificate('tempobot-406fc-firebase-adminsdk-o6bkq-a1ab9cdc76.json')
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://tempobot-406fc-default-rtdb.europe-west1.firebasedatabase.app/'
+})
 
-# Récupérer toutes les données de la base de données
-data = db.get()
+# récupérer une référence à l'emplacement de la base de données
+ref = db.reference('data/users/KP8dIxFtCBSshnxwRTSvHtskb5F2/prises/08F24CD6-DB99-4491-A5E4-DDD48B2283D6/isOn')
 
-# Récupérer les données d'un nœud spécifique de l'arbre
-node_data = db.child("data").get()
+# récupérer les données
+data = ref.get()
 
+def set_power(status):
+    if status == "ON":
+        response = requests.get(f"http://{ip}/cm?cmnd={command_on}")
+    elif status == "OFF":
+        response = requests.get(f"http://{ip}/cm?cmnd={command_off}")
+    else:
+        print("Erreur : statut invalide")
+        return
+
+    if response.status_code != 200:
+        print("Erreur : impossible de contrôler la prise Tasmota")
+    else:
+        print(f"Prise Tasmota : {status}")
+
+while True :
+    time.sleep(1)
+    ref = db.reference('data/users/KP8dIxFtCBSshnxwRTSvHtskb5F2/prises/08F24CD6-DB99-4491-A5E4-DDD48B2283D6/isOn')
+
+    # récupérer les données
+    data = ref.get()
+    if data==True :
+        set_power("ON")
+    else :
+        set_power("OFF")
+
+
+# afficher les données
+print(data)
